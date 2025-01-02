60a54596f398f64ab169a41fcf8897a94b6a72bb
Med Jam <elqajjam@gmail.com>
2025-01-02 18:13:51 +0000

    --

--- src/app/blockchain/history/history.component.ts
+++ src/app/blockchain/history/history.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TransactionService } from '../services/transaction.service';
 
 @Component({
-  selector: 'app-history',
-  imports: [],
-  templateUrl: './history.component.html',
-  styleUrl: './history.component.css'
-})
+    selector: 'app-history',
+    templateUrl: './history.component.html',
+    styleUrls: ['./history.component.css'],
+    standalone: true,  // Make this a standalone component
+    imports: [CommonModule],})
 export class HistoryComponent {
+  transactionHistory: { hash: string; amount: string; timestamp: Date, To: string }[] = [];
 
+  constructor(private transactionService: TransactionService) {
+    this.transactionHistory = this.transactionService.getTransactions();
+  }
 }
