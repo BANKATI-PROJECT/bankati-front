83249f7f058f88df020de6d63d365dddefe47ffe
Med Jam <elqajjam@gmail.com>
2025-01-03 16:14:56 +0000

    admin component + unauthorized comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,15 @@ import { AgentComponent } from './components/agent/agent.component';
 import { ClientComponent } from './components/client/client.component';
 import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';  // Import the UnauthorizedComponent
 import { AuthGuard } from './services/auth.guard';
+import { AddAgentComponent } from './components/admin/add-agent/add-agent.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/about', pathMatch: 'full' }, 
+
     { path: 'about', component: AboutComponent },
     { path: 'login', component: LoginComponent },
     { path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: { role: 'ADMIN' } },
+    {path: 'add-agent', component: AddAgentComponent, canActivate: [AuthGuard], data: { role: 'ADMIN' }},
     { path: 'agent', component: AgentComponent, canActivate: [AuthGuard], data: { role: 'AGENT' } },
     { path: 'client', component: ClientComponent, canActivate: [AuthGuard], data: { role: 'CLIENT' } },
     { path: 'unauthorized', component: UnauthorizedComponent },
--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AgentListComponent } from './agent-list/agent-list.component';
 
 @Component({
+  standalone: true,
   selector: 'app-admin',
-  imports: [],
+  imports: [AgentListComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
